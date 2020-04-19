fae08a3a0fa95ca9aa7890605c0ff88e776ee9b5
dnuwa <daniel.nuwa@gmail.com>
2020-04-19 21:43:58 +0300

    - add a details component - redirect appropriately

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Search from "./components/Searchform";
+import Details from "./components/Bookdetails";
 
 function App() {
   return (
-    <div className="App">
-      <Search />
-    </div>
+    <Router>
+      <div className="App">
+        <Route
+          exact
+          path="/"
+          render={(props) => (
+            <React.Fragment>
+              <Search />
+            </React.Fragment>
+          )}
+        />
+        <Route path="/book" component={Details} />
+      </div>
+    </Router>
   );
 }
 
